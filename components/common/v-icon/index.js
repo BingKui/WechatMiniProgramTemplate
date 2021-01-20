6329257af9cc79b22936bcafcc32b34b133c525d
BingKui <178974662@qq.com>
2021-01-20 14:39:10 +0800

    update

--- components/common/v-icon/index.js
+++ components/common/v-icon/index.js
@@ -1,23 +1,22 @@
-// components/base/v-icon/index.js
+// v-icon
 Component({
-    /**
-     * 组件的属性列表
-     */
     properties: {
-
+        name: {
+            type: String,
+            value: '',
+        },
+        size: {
+            type: Number,
+            value: 40,
+        },
+        color: {
+            type: String,
+            value: '',
+        },
     },
-
-    /**
-     * 组件的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 组件的方法列表
-     */
     methods: {
-
+        clickAction () {
+            this.triggerEvent('click');
+        },
     }
 })
